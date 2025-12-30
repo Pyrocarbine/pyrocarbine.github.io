2987f569b9c3e10784e6519e693d547238fd07a0
Pyrocarbine <aaronhan2006@gmail.com>
2025-12-29 23:48:15 -0800

    fixed favicon not showing

--- app/components/toolbar.tsx
+++ app/components/toolbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function ToolBar(){
     return (
     <div className="w-full overflow-x-auto">
         <div className="w-7/8 md:w-2/3 pt-2 flex flex-nowrap justify-start gap-10 ml-auto mr-auto sm:items-start">
-            <img src="../favicon.ico" className="rounded-lg" alt="Logo" width={64} height={64} />
+            <Image src="/images/favicon.ico" className="rounded-lg" alt="Logo" width={64} height={64} />
             <Link href="#Projects_title" className="inline-block text-black mt-auto mb-auto font-bold text-3xl text-bold decoration-solid decoration-blue-300 decoration-4 underline underline-offset-6 cursor-pointer"> Projects </Link>
             <Link href="#Skills_title" className="inline-block text-black mt-auto mb-auto font-bold text-3xl text-bold decoration-solid decoration-blue-300 decoration-4 underline underline-offset-6 cursor-pointer"> Skills </Link>
             <Link href="/files/Aaron_Han.pdf" target="_blank" className="inline-block text-black mt-auto mb-auto font-bold text-3xl text-bold decoration-solid decoration-blue-300 decoration-4 underline underline-offset-6 cursor-pointer"> Resume </Link>
